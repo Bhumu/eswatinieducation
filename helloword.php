9b8f012e1e27d96fc9d40bd002d1ff6f51905b1b
Bhumu <35570945+Bhumu@users.noreply.github.com>
2022-07-27 20:39:44 +0200

    Create helloword.php

---
+++ helloword.php
@@ -0,0 +1,13 @@
+<?Php
+//Go day today i am going to show you how to calculate age in php
+
+var year=date("Y");
+var datofbirth=2001;
+var age=0;
+
+$age=year-datofbirth;
+
+print("Your age this year ".date("Y")." is precisely ".$age);
+
+
+?>
